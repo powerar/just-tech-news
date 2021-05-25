1313a1f2d8570564f22f3da30c964e28f0759d9e
powerar <arpower@bell.net>
2021-05-25 13:27:07 -0400

    added dashboard functionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,10 @@
 module.exports = {
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
-    ).getDate()}/${new Date(date).getFullYear()}`;
+    ).getFullYear()}`;
   },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-
-    return word;
-  },
-  format_url: (url) => {
+  format_url: url => {
     return url
       .replace('http://', '')
       .replace('https://', '')
@@ -19,4 +12,11 @@ module.exports = {
       .split('/')[0]
       .split('?')[0];
   },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  }
 };
